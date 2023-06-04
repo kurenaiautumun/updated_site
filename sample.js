a6fcb1202fb3a8f8e4abca0263ab3d338125c8dd
Abhinav <abhinavchaudhary10@gmail.com>
2023-06-04 15:53:10 +0530

    testing

--- sample.js
+++ sample.js
@@ -14,7 +14,7 @@ app.post("/gitTrigger", function(req, res){
     data = {}
     data["method"] = "GET"
     console.log("before trigger")
-    exec('git pull', (err, stdout, stderr) => {
+    exec('git pull; npm i', (err, stdout, stderr) => {
       if (err) {
         console.log("command could not be exec")
         return;
